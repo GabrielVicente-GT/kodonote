24f75265e5b3849b451029d5f5ad15f4b34459bd
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2022-04-21 23:13:47 -0600

    prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import EditorContainer from "./EditorContainer"
+import App from "./App.js"
 
 ReactDOM.render(
     <EditorContainer />,
