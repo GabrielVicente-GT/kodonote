c95554671fd0770571cbd7541394dae60d4e246f
unknown <staracenapuga@gmail.com>
2022-05-13 10:44:03 -0600

    Correciones en MainMenu.js y ProtectedRoute.js.

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
 import "./styles/App.css"
 import GridContainer from "./components/ContainerNB.js"
 import Options from "./components/options.js"
@@ -7,21 +6,19 @@ import { useUserAuth } from "./auth/UserAuthContext"
 
 const MainMenu = () => {
 
-    // Obtener metodos del proveedor de contexto
-    // logOut se manda a opciones para cerrar la sesión
-    const { user, logOut } = useUserAuth()
+  const { user, logOut } = useUserAuth()
 
-    return (
-      <div>
-        <header className="header">
-          <h2 className="title">{user ? "Hola de nuevo " + user?.email + "!" : "Kodonote"}</h2>
-        </header>
-        <div className="content">
-          <Options logOut={logOut}/>
-          <GridContainer />
-        </div>
+  return (
+    <div>
+      <header className="header">
+        <h2 className="title">{user ? "Hola de nuevo " + user?.email + "!" : "Kodonote"}</h2>
+      </header>
+      <div className="content">
+        <Options logOut={logOut}/>
+        <GridContainer />
       </div>
-    );
+    </div>
+  )
 }
 
 export default MainMenu
