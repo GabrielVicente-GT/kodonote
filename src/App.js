a524ceeb52a44dd10a377d97c91a56511afbd67a
unknown <staracenapuga@gmail.com>
2022-08-19 21:12:10 -0600

    Archivos innecesarios eliminados.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => (
           path="/editor"
           element={
             <ProtectedRoute>
-              <Annotations/>
+              <Annotations />
             </ProtectedRoute>
           }
         />
