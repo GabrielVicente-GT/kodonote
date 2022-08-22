88258ffeefd18cc044bab77ec2db48bc9f8fc617
unknown <staracenapuga@gmail.com>
2022-08-21 18:22:52 -0600

    Layout y estilos básicos finalizados.

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo-negative.png'
+import '../styles/LandingPage.css'
 
 const LandingPage = () => (
   <div className="landing-page-container">
     <header className="landing-page-header">
       <div className="landing-page-header-logo">
         <img className="landing-page-logo" src={Logo} alt="Logo de Kodonote" />
-        <h1>Annotation App</h1>
+        <h1>Kodonote</h1>
+      </div>
+      <div className="landing-page-header-link">
+        <Link className="landing-page-link" to="/login">Ingresa a Kodonote</Link>
       </div>
     </header>
     <main className="landing-page-main">
-      <h1>Landing Page</h1>
-      <span>
-        Inicia sesión dando click {' '}
-        <Link to="/login">aquí</Link>
-      </span>
+      <div className="landing-page-card">
+        <h1>Landing Page</h1>
+        <span>
+          Inicia sesión dando click {' '}
+          <Link to="/login">aquí</Link>
+        </span>
+      </div>
     </main>
   </div>
 )
