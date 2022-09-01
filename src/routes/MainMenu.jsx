1b29b2801a69276fdbe3f066135dd94647eee5c1
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-08-31 21:25:44 -0600

    Popups finalizados

--- src/routes/MainMenu.jsx
+++ src/routes/MainMenu.jsx
@@ -108,8 +108,7 @@ const MainMenu = () => {
         <h2 className="title">
           {user ? `Hola de nuevo ${user?.email}!` : 'Kodonote'}
         </h2>
-      </header>
-      
+      </header> 
       <AccoPopUp style={popupActiveAccount} activePopup={activePopupAccount}/> 
       <ConfigPopUp style={popupActiveSetting} activePopup={activePopupSettings}/>
       <div className="content">
