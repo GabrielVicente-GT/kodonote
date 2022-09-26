89d4333d76be8d48276d3f6102d301f6f92dbd53
unknown <staracenapuga@gmail.com>
2022-09-26 11:40:22 -0600

    Layout de la página de compra de temas finalizado.

--- src/routes/ThemesPage.jsx
+++ src/routes/ThemesPage.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo-negative.png'
+import themes from '../utils/themeGetter'
+import '../styles/ThemesPage.css'
 
-const ThemesPage = () => (
-  <header>
-    Hello!
-  </header>
-)
+const ThemesPage = () => {
+  const [purchasedTheme, setPurchasedTheme] = useState()
+  const [onPurchaseProcess, setOnPurchaseProcess] = useState(false)
+
+  return (
+    <>
+      <header>
+        <h2>Observa y compra temas para Kodonote</h2>
+      </header>
+      <aside>
+        <img src={Logo} alt="Logo de Kodonote" />
+        <input
+          type="text"
+          name="theme-search-bar"
+          id="theme-search-bar"
+          className="theme-search-bar"
+          placeholder="Buscar tema"
+        />
+        <button type="button" className="theme-aside-button">Ver mis temas</button>
+        <Link to="/main" className="theme-aside-button">Volver</Link>
+      </aside>
+      <main>
+        {themes.map((theme) => (
+          <div className="theme-card">
+            <img src={theme.source} alt="Tema de Kodonote" />
+            <div className="theme-info">
+              <div className="theme-title">
+                <h3>{theme.title}</h3>
+              </div>
+              <div className="theme-purchase-info">
+                <span>{theme.price}</span>
+                <div className="theme-purchase-button">
+                  <button type="button" onClick={() => {
+                    setPurchasedTheme(theme)
+                    setOnPurchaseProcess(true)
+                  }}>Comprar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </main>
+      {(onPurchaseProcess) ? (
+        null
+      ) : (
+        null
+      )}
+    </>
+  )
+}
 
 export default ThemesPage
---
+++ src/utils/themeGetter.jsx
@@ -0,0 +1,35 @@
+import Theme01 from '../images/themes/theme-01.jpg'
+import Theme02 from '../images/themes/theme-02.jpg'
+import Theme03 from '../images/themes/theme-03.jpg'
+import Theme04 from '../images/themes/theme-04.jpg'
+import Theme05 from '../images/themes/theme-05.jpg'
+
+const themes = [
+  {
+    source: Theme01,
+    title: 'Mountains at sunset',
+    price: '$.0.99',
+  },
+  {
+    source: Theme02,
+    title: 'Cold mount at midnight',
+    price: '$.0.99',
+  },
+  {
+    source: Theme03,
+    title: 'Distant blue forest',
+    price: '$.0.99',
+  },
+  {
+    source: Theme04,
+    title: 'Someone in the distance',
+    price: '$.0.99',
+  },
+  {
+    source: Theme05,
+    title: 'Sunrise at the lake',
+    price: '$.0.99',
+  },
+]
+
+export default themes
