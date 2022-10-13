337231f73b939ac012e1ee0cee73081bbc41dff2
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2022-10-13 10:47:49 -0600

    integracion de pagina de ver mis temas

--- src/routes/ThemesPage.jsx
+++ src/routes/ThemesPage.jsx
@@ -33,7 +33,7 @@ const ThemesPage = () => {
           className="theme-search-bar"
           placeholder="Buscar tema"
         />
-        <button type="button" className="theme-aside-button">Ver mis temas</button>
+        <Link to="/my-themes" className="theme-aside-button">Ver mis temas</Link>
         <Link to="/main" className="theme-aside-button">Volver</Link>
       </aside>
       <main className="themes-main">
