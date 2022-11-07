319a4598db1e74c2780a29d3642b2ce73c29684f
Gabriel <chentevilo2001@gmail.com>
2022-11-06 21:57:23 -0600

    switch display tested negation

--- src/tests/switchDisplay.test.js
+++ src/tests/switchDisplay.test.js
@@ -4,4 +4,7 @@ describe('Dada una opción para desplegar el menú', () => {
     it('Despliega el menú como grid si esa es la opción', () => {
         expect(switchDisplay('grid')).toStrictEqual({ width: '100%', height: '100vh', displayMode: 'grid' })
     })
+    it('No despliega el menú como grid si esta no es su opción', () => {
+        expect(switchDisplay('flex')).not.toStrictEqual({ width: '100%', height: '100vh', displayMode: 'grid' })
+    })
 })
