1d51e9a98ef8e49de726de9a2566d376dc128a59
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2022-11-06 20:07:46 -0600

    test

--- src/tests/popUp.test.jsx
+++ src/tests/popUp.test.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom' // Necesario para los test de cosas como -> que se renderice el texto correcto
 import userEvent from '@testing-library/user-event' // Eventos como dar click y otras cosas de usuario
-<<<<<<< HEAD
-import MainMenuMock from '../routes/MainMenu'
-=======
 import MainMenuMock from '../utils/MainMenuMock'
->>>>>>> popup-test
 import 'regenerator-runtime/runtime'// Async Await
 
 describe('Given a string -> string is a operator', () => {
