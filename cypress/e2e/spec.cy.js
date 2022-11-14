1976600a286f5893d2230db396f5123ba40ce826
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-11-13 22:37:08 -0600

    Test base pruebas de sistema

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+      cy.visit('http://localhost:3000/login') // change URL to match your dev URL
+      
+      cy.get('#Email').type('christopherg2808@gmail.com')
+      cy.get('#password').type('Chris1910*')
+
+      cy.get('.form-btn-is').click()
+
+  })
+})
